fc75c89cf5f2e36d31c1df4b5deea75e89fb1707
Rodrigo Dlugokenski <rddweb@gmail.com>
2011-10-18 14:17:02 -0200

    Esqueleto inicial de um projeto Android

---
+++ src/com/android/mechawars/MechawarsActivity.java
@@ -0,0 +1,13 @@
+package com.android.mechawars;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MechawarsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
